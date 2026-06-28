b94ea737d77fd62b4107b402bce7c8c3bd5475e2
akshat_1074 <valaniakshat1074@gmail.com>
2026-06-28 11:31:43 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-black">Go to onboarding</Link>
+    </View>
+
+  );
+}
